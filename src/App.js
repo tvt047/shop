e2d4db22903428117c14a94839d7f01724a6220f
tvt047 <36876251+tvt047@users.noreply.github.com>
2023-07-15 21:15:33 +0530

    Added Shop name with colour

--- src/App.js
+++ src/App.js
@@ -9,6 +9,9 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <div>
+          <h1 style={{color: "red"}}>This is TVT</h1>
+        </div>
         <a
           className="App-link"
           href="https://reactjs.org"
